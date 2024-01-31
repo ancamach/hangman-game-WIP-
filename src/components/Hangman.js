97211df84dc5b61856393be1fd25cebd832777a3
ancamach <andrea.cam.ag@gmail.com>
2024-01-31 13:56:50 +0100

    dont know what i changed

--- src/components/Hangman.js
+++ src/components/Hangman.js
@@ -17,7 +17,7 @@ const HangmanGame = () => {
     dispatch(checkGameOver());
   };
 
-  const letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' ', 'Á', 'É', 'Í', 'Ó', 'Ú'];
+  const letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' ', 'Á', 'É', 'Í', 'Ó', 'Ú', 'Ü'];
 
   return (
     <div>
